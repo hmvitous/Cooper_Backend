54e727dae77303e7b86b6729229dcd25927c9089
Hunter <hmvitous@yahoo.com>
2020-02-26 10:45:52 +0100

    Creates a model to save user data and creates test for said model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class  User < ActiveRecord::Base
     extend Devise::Models
 
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-    include DeviseTokenAuth::Concerns::User 
+    include DeviseTokenAuth::Concerns::User
+    has_many :performance_data, class_name: 'PerformanceData'
 end
