372af02c4b0e22c1db90edd8133c9fb671dfb9fb
Janko <mailodjanka@gmail.com>
2020-02-26 11:02:48 +0100

    Added performance data spec file that will test if the user entry will be saved. Modified routes to include new route pointing to performance data controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   
   namespace :api do
     
-    namespace :v1 do
+  namespace :v1, defaults: { format: :json} do
+
+    resources :performance_data, only: [:create]
     end
   end
 end
